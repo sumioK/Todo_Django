456126fc81979bfb259892cbc7ae6f6ec9a23843
sumiok <tap.miwo@gmail.com>
2022-12-18 15:47:29 +0900

    success to migration

--- todo/models.py
+++ todo/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class TodoModel(models.Model):
-  title = models.Charfield(max_length=100)
+  title = models.CharField(max_length=100)
   memo = models.TextField()
