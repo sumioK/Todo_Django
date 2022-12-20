bf7505362bc1130d193b9589df970e07bf9b52d6
sumiok <tap.miwo@gmail.com>
2022-12-21 05:49:06 +0900

    create template

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.view.generic.list import ListView
+from .models import TodoModel
 # Create your views here.
+
+class TodoList(ListView):
+  template_name = 'list.html'
+  model = TodoModel
